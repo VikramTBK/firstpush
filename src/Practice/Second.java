6f083beaca70b5ec1e4566c3781d39efc2710981
Vikram <vikramtbk@protonmail.com>
2022-04-28 17:22:35 +0530

    First Push

---
+++ src/Practice/Second.java
@@ -0,0 +1,23 @@
+package Practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Second {
+
+	public static void main(String[] args) {
+
+		HashMap<Integer, String> mp = new HashMap<Integer, String>();
+		mp.put(1, "Alpha");
+		mp.put(2, "Beta");
+		mp.put(3, "Gama");
+
+		System.out.println("Iterating HashMap...");
+		for (Map.Entry m : mp.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+
+		}
+
+	}
+
+}
